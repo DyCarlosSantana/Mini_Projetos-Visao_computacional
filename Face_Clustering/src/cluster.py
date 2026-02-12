418de7672dc40cd82bad359d4c0f90e9222c3de8
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-12 16:31:02 -0300

    update cluster.py
    Adicionando um dicionario para coletar quantos rostos/pessoas foram analisados; um contador antes do loop principal encerrar.
    Importação da biblioteca 'datetime', e geração de relatorio automatico no final do processo.

--- Face_Clustering/src/cluster.py
+++ Face_Clustering/src/cluster.py
@@ -1,6 +1,7 @@
 from deepface import DeepFace
 import shutil
 import os
+from datetime import datetime
 
 imagens_path = './assets/raw'
 resultados_path = './assets/processed'
@@ -11,6 +12,7 @@ print(f'Encontrei os seguintes arquivos:')
 print(img_list )
 
 identidades_conhecidas = []
+stats = {} # Dicionário vazio para guardar a contagem (ex: {'Pessoa_0': 5, 'Pessoa_1': 3})
 
 for arquivo in img_list:
     if not arquivo.lower().endswith(('.png', '.jpg', '.jpeg')): # Estrutura usada em python para verificar se uma string (geralmente um nome de arquivo) NÃO termina com uma extensão ou sufixo especifico.
@@ -38,12 +40,14 @@ for arquivo in img_list:
         # Ajuda muito a entender por que ele separou as pastas, podemos analisar a distancia 
         print(f"[Debug] Comparando com Pessoa_{index}: Distância = {distancia:.4f}")
 
-        if distancia < 0.65:
+        if distancia < 0.60:
+            print(f'Achamos! E a pessoa_{index}')
             nome_pasta = f"Pessoa_{index}"
             encontrou_id = True
             break
 
     if not encontrou_id:
+        print('Nova Identidade!')
         novo_index = len(identidades_conhecidas)
         nome_pasta = f"Pessoa_{novo_index}"
         identidades_conhecidas.append(emb_atual)
@@ -55,5 +59,27 @@ for arquivo in img_list:
     # Movemos ou copiamos o arquivo da pasta 'raw' para a pasta da pessoa correspondente
     shutil.copy(caminho_completo, os.path.join(caminho_destino_pasta, arquivo))
     print(f"-> {arquivo} movido para {nome_pasta}")
+    # Se a pessoa já existe no dicionário, soma +1. Se não, começa com 0 e soma +1.
+    stats[nome_pasta] = stats.get(nome_pasta, 0) + 1
 
 print('Processo Concluido!')
+
+# --- NOVO BLOCO: GERAR RELATÓRIO TXT ---
+caminho_relatorio = os.path.join(resultados_path, "relatorio_final.txt")
+
+print("\n📝 Gerando relatório...")
+
+with open(caminho_relatorio, "w", encoding="utf-8") as f:
+    f.write("=== RELATÓRIO DE ORGANIZAÇÃO ===\n")
+    f.write(f"Data de Execução: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\n")
+    f.write("================================\n\n")
+    
+    f.write(f"Total de Identidades Encontradas: {len(identidades_conhecidas)}\n")
+    f.write(f"Total de Imagens Processadas: {sum(stats.values())}\n\n")
+    
+    f.write("--- Detalhe por Pessoa ---\n")
+    # Loop para escrever linha por linha quem é quem
+    for pessoa, quantidade in stats.items():
+        f.write(f"- {pessoa}: {quantidade} imagens\n")
+        
+print(f"✅ Relatório salvo em: {caminho_relatorio}")
