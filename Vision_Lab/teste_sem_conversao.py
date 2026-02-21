2011f075c675c4b56c2a124e39dc8fdddb8a85e6
Edy Carlos Santana <dy.carlos.santana@gmail.com>
2026-02-21 01:42:07 -0300

    Create teste_sem_conversao.py
    script de teste realizado para ver o resultado de uma imagem sem o processo de  conversão BGR -> RGB para a exibição usando a biblioteca matplotlib

---
+++ Vision_Lab/teste_sem_conversao.py
@@ -0,0 +1,51 @@
+import cv2     # OpenCV - Para processamento de imagens
+import numpy as np  # NumPy - Para trabalhar com arrays de forma eficiente caso necessario
+import matplotlib.pyplot as plt  # Matplotlib - Para exibir imagens na tela
+# import 'nome da biblioteca' as 'apelido'
+
+IMG_PATH = 'assets/foto.jpg'
+
+
+def carregar_imagem(caminho):
+    """
+    Carrega do disco.
+    Args:
+        caminho (str): Caminho do arquivo de imagem
+    Returns:
+        numpy.ndarray: Imagem carregada em fomato BGR ou None se o carregamento falhar.
+    """
+    img = cv2.imread(caminho)
+    if img is None:
+        print(f"Erro: Não foi possivel carregar {caminho}")
+        return None
+    
+    print(f"Imagem Carregada: {img.shape}") # .shape para mostrar as dimensões da Imagem em uma tupla (altura, largura, canais)
+    return img
+
+
+def exibir_imagem_original(imagem):
+    """
+    Exibir uma imagem em formato BGR
+    Arg:
+        imagem: (numpy.ndarray): Imagem em formato BGR (opencv)
+    """
+    if imagem is None:
+        print("⚠️ Imagem Invalida! Nada a exibir")
+        return
+    
+    #img_rgb = cv2.cvtColor(imagem, cv2.COLOR_BGR2RGB)
+    plt.imshow(imagem)
+    plt.axis('off')  # Remove os eixos (números) da imagem
+    plt.title('Imagem Original - RGB')  # Adiciona título
+    plt.show()
+
+print("Iniciando Vision Lab...")
+
+imagem = carregar_imagem(IMG_PATH)
+if imagem is None:
+    print("⚠️ Não foi possível processar a imagem. Encerrando.")
+else:
+    print("Exibindo imagem original...")
+    exibir_imagem_original(imagem)
+    
+    print("✅ Processamento concluído!")
